4324dce84623598b8b5c0e0ba7230de2e1c75c32
Matheus <ymatheus.h@gmail.com>
2024-02-09 21:18:47 -0300

    feat: :sparkles: create QuestionRepository

---
+++ src/main/java/tech/ymaatheus/certification_nlw/modules/questions/repositories/QuestionRepository.java
@@ -0,0 +1,14 @@
+package tech.ymaatheus.certification_nlw.modules.questions.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tech.ymaatheus.certification_nlw.modules.questions.entities.QuestionEntity;
+
+public interface QuestionRepository extends JpaRepository<QuestionEntity, UUID> {
+
+  List<QuestionEntity> findByTechnology(String technology);
+  
+}
