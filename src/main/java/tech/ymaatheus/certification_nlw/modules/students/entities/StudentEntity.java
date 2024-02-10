1683e9ab70413782b7903087b940e7478fbcf9a3
Matheus <ymatheus.h@gmail.com>
2024-02-09 21:15:52 -0300

    feat: :sparkles: create entities student, certification student, answers certitifications

---
+++ src/main/java/tech/ymaatheus/certification_nlw/modules/students/entities/StudentEntity.java
@@ -0,0 +1,38 @@
+package tech.ymaatheus.certification_nlw.modules.students.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "students")
+public class StudentEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(unique = true, nullable = false)
+  private String email;
+
+  @OneToMany(mappedBy = "studentEntity")
+  private List<CertificationStudentEntity> certificationStudentEntity;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+}
