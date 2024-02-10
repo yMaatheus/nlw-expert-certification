0646cd195fdb2f4596ede4483a1739f0001e18d2
Matheus <ymatheus.h@gmail.com>
2024-02-09 21:14:40 -0300

    feat: :sparkles: Create QuestionAnswerDTO

---
+++ src/main/java/tech/ymaatheus/certification_nlw/modules/students/dto/QuestionAnswerDTO.java
@@ -0,0 +1,17 @@
+package tech.ymaatheus.certification_nlw.modules.students.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class QuestionAnswerDTO {
+  
+  private String questionID;
+  private String alternativeID;
+  
+}
